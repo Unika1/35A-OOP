b1d54f2752019cd333f44a7850dbe5021be6d180
unika1 <unikaprj@gmail.com>
2024-04-11 20:08:09 +0545

    Week1 task5

--- Week1/task5.java
+++ Week1/task5.java
@@ -1,6 +1,15 @@
 package Task.Week1;
 
 public class task5 {
+    /*5. WAP prints the following pattern:
+
+*********
+
+*********
+
+*********
+
+********* */
     public static void main(String[] args) {
         System.out.println("*********");
         System.out.println("*********");
