7e128f688915d6594209339a0fee0d30000bc61d
unika1 <unikaprj@gmail.com>
2024-04-11 10:11:28 +0545

    Week 1 started

---
+++ Week1/Hello.java
@@ -0,0 +1,9 @@
+package Task.Week1;
+
+public class Hello {
+    public static void main(String[] args) {
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
+        System.out.println("Hello World!");
+       
+    }
+}
