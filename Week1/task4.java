12cf07e6ca2e7db9e37bb3f3106d635d223f3d09
unika1 <unikaprj@gmail.com>
2024-04-11 11:10:11 +0545

    Week1 task4

---
+++ Week1/task4.java
@@ -0,0 +1,11 @@
+package Task.Week1;
+
+public class task4 {
+    
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
